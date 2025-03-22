c4b3f629cb8c9b5d9661299517cb0e47ed3c5cac
SNBeast <snburchett@gmail.com>
2025-03-22 13:46:57 -0400

    Darwin target

--- build/afterpack.js
+++ build/afterpack.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
+const download = require('electron-download');
+const JSZip = require('jszip'); // the one with easier renaming...
+const AdmZip = require('adm-zip'); // and the one with synchronous file reading
+
 const defaultdir = './dist/win-ia32-unpacked/resources/default_app'
 const exefile = './dist/win-ia32-unpacked/OpenATBPClient.exe'
+const macfile = './dist/OpenATBPClient-1.0.0-x64-mac.zip';
+const resources = './dist/win-ia32-unpacked/resources/';
+const license = './dist/win-ia32-unpacked/LICENSE.md';
+const unityinstaller = './build/webplayer-mini.dmg';
+const macicon = './build/icon.icns';
+const unitylicense = './build/utils/LICENSE.WEBPLAYER.md';
+const unity3xxmac = './build/UnityPlayer3.x.x-x86_64.bundle.zip';
 
 exports.default = function() {
   // remove leftover files from default electron app
@@ -23,6 +34,99 @@ exports.default = function() {
         );
     } else {
         throw err;
+    }
+  });
+
+  // code from here is our Mac build process
+
+  // the raw way to do a rename, because there's nothing better
+  const renameSingle = function (zip, oldPath, newPath) {
+    zip.files[oldPath].name = newPath;
+    zip.files[oldPath].unsafeOriginalName = newPath;
+    zip.files[newPath] = zip.files[oldPath];
+    delete zip.files[oldPath];
+  };
+
+  const renameFolder = function (zip, oldPath, newPath) {
+    if (oldPath.slice(-1) !== "/") {
+      oldPath += "/";
+    }
+    if (newPath.slice(-1) !== "/") {
+      newPath += "/";
+    }
+    zip.filter((_relPath, fileObject) => fileObject.name.slice(0, oldPath.length) === oldPath)
+      .forEach(fileObject => renameSingle(zip, fileObject.name, fileObject.name.replace(oldPath, newPath)));
+  };
+
+  download({
+    version: '0.31.2',
+    arch: 'x64',
+    platform: 'darwin'
+  }, (err, originalZipPath) => {
+    if (!err) {
+      fs.copyFileSync(originalZipPath, macfile);
+      JSZip.loadAsync(fs.readFileSync(macfile)).then(function (zip) {
+        zip.file('Electron.app/Contents/Info.plist').async("string").then(function (data) {
+          var unityZip = new AdmZip(unity3xxmac);
+          unityZip.forEach(fileObject => {
+            if (!fileObject.isDirectory) {
+              zip.file('Electron.app/Contents/Resources/Stable' + fileObject.entryName, fileObject.getData(), {createFolders: true, unixPermissions: fileObject.attr});
+            }
+          });
+
+          zip
+            .file('Electron.app/Contents/Info.plist', data
+              .replace("<key>CFBundleDisplayName</key>\n\t<string>Electron</string>", "<key>CFBundleDisplayName</key>\n\t<string>OpenATBPClient</string>")
+              .replace("<key>CFBundleExecutable</key>\n\t<string>Electron</string>", "<key>CFBundleExecutable</key>\n\t<string>OpenATBPClient</string>")
+              .replace("<key>CFBundleName</key>\n\t<string>Electron</string>", "<key>CFBundleName</key>\n\t<string>OpenATBPClient</string>")
+              .replace("<key>CFBundleIdentifier</key>\n\t<string>com.github.electron</string>", "<key>CFBundleIdentifier</key>\n\t<string>xyz.openatbp.client</string>")
+              .replace("<key>CFBundleIconFile</key>\n\t<string>atom.icns</string>", "<key>CFBundleIconFile</key>\n\t<string>atbp-bee.icns</string>")
+            )
+            .file("webplayer-mini.dmg", fs.readFileSync(unityinstaller))
+            .file("LICENSE.WEBPLAYER.md", fs.readFileSync(unitylicense))
+            .file("Electron.app/Contents/Resources/atbp-bee.icns", fs.readFileSync(macicon))
+            .file("README.txt",
+              'Before running OpenATBPClient for the first time, the following two things should be done:\n' +
+              '\t1. Install the bundled Unity Web Player (webplayer-mini.dmg), if Unity Web Player is not already installed. (If unsure, OpenATBPClient will tell you if it is not installed.)\n' +
+              '\t2. Control-Click -> Open OpenATBPClient and open it to remove its signature warning.\n' +
+              '\t\t- If that does not work, run the following command in this directory in Terminal:\n' +
+              '\t\t\txattr -r -d com.apple.quarantine OpenATBPClient.app\n\n' +
+
+              'After those are completed, you may move OpenATBPClient wherever you wish (like Applications) and discard the rest of the files.\n'
+            );
+
+          fs.readdirSync(resources, {recursive: true}).forEach(filename => {
+            if (!fs.lstatSync(resources + filename).isDirectory() && filename != "atom.asar" && filename != "elevate.exe") {
+              zip.file("Electron.app/Contents/Resources/" + filename.replaceAll("\\", "/"), fs.readFileSync(resources + filename), {createFolders: true});
+            }
+          });
+
+          zip
+            .remove("version")
+            .remove("Electron.app/Contents/Resources/atom.icns")
+            .remove("Electron.app/Contents/Resources/default_app")
+            .folder(/Electron\.app\/Contents\/Resources\/.*\.lproj/).forEach(fileObject => zip.remove(fileObject.name));
+
+          renameSingle(zip, "Electron.app/Contents/MacOS/Electron", "Electron.app/Contents/MacOS/OpenATBPClient");
+          renameSingle(zip, "LICENSE", "LICENSE.electron.txt");
+          renameFolder(zip, "Electron.app", "OpenATBPClient.app");
+
+          zip
+            .generateNodeStream({compression: 'DEFLATE', type: 'nodebuffer', streamFiles: 'true', platform: 'UNIX'})
+            .pipe(fs.createWriteStream(macfile))
+            .on('finish', function () {
+              // now we need to pack the zip in another zip to bypass a Safari bug
+              new JSZip().file('OpenATBPClient-1.0.0-x64-mac.zip', fs.readFileSync(macfile)).file("LICENSE.md", fs.readFileSync(license))
+                .generateNodeStream({compression: 'DEFLATE', type: 'nodebuffer', streamFiles: 'true', platform: 'UNIX'})
+                .pipe(fs.createWriteStream(macfile))
+                .on('finish', function () {
+                  console.log("Created Darwin build.");
+                });
+            });
+        });
+      });
+    } else {
+      throw err;
     }
   });
 }
--- index.js
+++ index.js
@@ -3,47 +3,123 @@ var fs = require("fs-extra");
 var os = require("os");
 var dialog = require("dialog");
 var BrowserWindow = require("browser-window");
+var ChildProcess = require("child_process");
+var shell = require("shell");
 
 var mainWindow = null;
+var config = null;
 var initialPageLoad = false;
+var disableHTTPS = false;
+
+var pathSeparator = process.platform == "win32" ? "\\" : "/";
+var configPath = app.getPath("userData") + pathSeparator + "config.json";
 
 app.commandLine.appendSwitch("--enable-npapi");
 
-function verifyUnity() {
-    var dllpath =
-        app.getPath("appData") +
-        "\\..\\LocalLow\\Unity\\WebPlayer\\player\\3.x.x\\webplayer_win.dll";
-
-    if (fs.existsSync(dllpath)) {
-        var buff = fs.readFileSync(dllpath);
-        var hash = require("crypto")
-            .createHash("md5")
-            .update(buff)
-            .digest("hex");
-        if (hash == "33ffd00503b206260b0c273baf7e122e") {
-            return true;
-        }
-    }
-    return false;
+function readU32BE(buffer, index) {
+    return (
+        ((buffer[index] << 24) |
+            (buffer[index + 1] << 16) |
+            (buffer[index + 2] << 8) |
+            buffer[index + 3]) >>>
+        0
+    ); // rotation by zero converts to unsigned
 }
 
-function installUnity(callback) {
-    var utilsdir = __dirname + "\\..\\..\\utils";
+function ensureUnity(callback) {
+    if (process.platform == "win32") {
+        var utilsdir = process.env.npm_node_execpath
+            ? app.getAppPath() + "\\build\\utils"
+            : __dirname + "\\..\\..\\utils";
 
-    // if running in non-packaged / development mode, this dir will be slightly different
-    if (process.env.npm_node_execpath) {
-        utilsdir = app.getAppPath() + "\\build\\utils";
+        // verify
+        var dllpath =
+            app.getPath("appData") +
+            "\\..\\LocalLow\\Unity\\WebPlayer\\player\\3.x.x\\webplayer_win.dll";
+
+        if (fs.existsSync(dllpath)) {
+            var buff = fs.readFileSync(dllpath);
+            var hash = require("crypto")
+                .createHash("md5")
+                .update(buff)
+                .digest("hex");
+            if (hash == "33ffd00503b206260b0c273baf7e122e") {
+                return callback(); // it's here, no need to install
+            }
+        }
+
+        // run the installer silently
+        var child = ChildProcess.spawn(utilsdir + "\\UnityWebPlayer.exe", [
+            "/quiet",
+            "/S",
+        ]);
+        child.on("exit", function () {
+            console.log("Unity Web Player installed successfully.");
+            return callback();
+        });
+    } else if (process.platform == "darwin") {
+        // make sure that the base plugin is 64-bit
+        var pluginpath = "/Library/Internet Plug-Ins/Unity Web Player.plugin";
+        var playerpath =
+            pluginpath +
+            "/Contents/Frameworks/StableUnityPlayer3.x.x-x86_64.bundle";
+        var installed = false;
+        if (fs.existsSync(pluginpath + "/Contents/MacOS/Unity Web Player")) {
+            var buff = fs.readFileSync(
+                pluginpath + "/Contents/MacOS/Unity Web Player"
+            );
+            // is a 64-bit single-arch Mach-O...
+            if (readU32BE(buff, 0) == 0xfeedfacf) {
+                // for x86_64
+                installed = readU32BE(buff, 4) == 0x01000007;
+            }
+            // or is a multi-arch Mach-O...
+            else if (readU32BE(buff, 0) == 0xcafebabe) {
+                var archs = readU32BE(buff, 4);
+
+                // where one of the arches...
+                for (var i = 0; i < archs; i++) {
+                    // is x86_64
+                    if (readU32BE(buff, 20 * i + 8) == 0x01000007) {
+                        installed = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if (!installed) {
+            // the base plugin can't be automatically installed on Darwin, so the user will have to do it for us
+            dialog.showErrorBox(
+                "Error!",
+                'Unity Web Player is not installed.\n\nPlease install the bundled Unity Web Player ("webplayer-mini.dmg"), then restart this app.'
+            );
+            app.quit();
+        }
+
+        if (fs.existsSync(playerpath)) {
+            // no real need to check further
+            return callback();
+        } else {
+            if (process.env.npm_node_execpath) {
+                dialog.show(
+                    "Info",
+                    'NPM run doesn\'t support auto-installing bundles. If load fails, unquarantine "build/UnityPlayer3.x.x-x86_64.bundle.zip" and extract as "' +
+                        playerpath +
+                        '".'
+                );
+            } else {
+                fs.copySync(
+                    __dirname + "/../StableUnityPlayer3.x.x-x86_64.bundle",
+                    playerpath
+                );
+            }
+        }
+
+        return callback();
+    } else {
+        // Unity Web Player doesn't support other platforms, so good luck!
+        return callback();
     }
-
-    // run the installer silently
-    var child = require("child_process").spawn(
-        utilsdir + "\\UnityWebPlayer.exe",
-        ["/quiet", "/S"]
-    );
-    child.on("exit", function () {
-        console.log("Unity Web Player installed successfully.");
-        callback();
-    });
 }
 
 function initialSetup(firstTime) {
@@ -56,12 +132,16 @@ function initialSetup(firstTime) {
         frame: false,
     });
     setupWindow.loadUrl("file://" + __dirname + "/initialsetup.html");
-    installUnity(function () {
+    ensureUnity(function () {
         if (firstTime) {
             // Copy default config
             fs.copySync(
-                __dirname + "\\defaults\\config.json",
-                app.getPath("userData") + "\\config.json"
+                __dirname +
+                    pathSeparator +
+                    "defaults" +
+                    pathSeparator +
+                    "config.json",
+                configPath
             );
         }
         setupWindow.destroy();
@@ -75,14 +155,34 @@ app.on("window-all-closed", function () {
 });
 
 app.on("ready", function () {
-    // Check just in case the user forgot to extract the zip.
-    zip_check = app.getPath("exe").includes(os.tmpdir());
-    if (zip_check) {
-        errormsg =
-            "It has been detected that OpenATBPClient is running from the TEMP folder.\n\n" +
-            "Please extract the entire Client folder to a location of your choice before starting OpenATBPClient.";
-        dialog.showErrorBox("Error!", errormsg);
-        return;
+    if (process.platform == "darwin") {
+        var full_osx_version = ChildProcess.execSync("sw_vers -productVersion")
+            .toString()
+            .trim()
+            .split(".");
+        var osx_release =
+            full_osx_version[0] == "10"
+                ? Number(full_osx_version[1])
+                : Number(full_osx_version[0]) + 5; // + 5 to cause Big Sur and up to follow Catalina as they should instead of overlapping El Capitan
+        switch (osx_release) {
+            // this Electron version states support for Mountain Lion (10.8), but the app derefs a null pointer there
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                disableHTTPS = true;
+                break;
+        }
+    } else {
+        // Check just in case the user forgot to extract the zip.
+        zip_check = app.getPath("exe").includes(os.tmpdir());
+        if (zip_check) {
+            errormsg =
+                "It has been detected that OpenATBPClient is running from the TEMP folder.\n\n" +
+                "Please extract the entire Client folder to a location of your choice before starting OpenATBPClient.";
+            dialog.showErrorBox("Error!", errormsg);
+            return;
+        }
     }
 
     // Create the browser window.
@@ -95,17 +195,12 @@ app.on("ready", function () {
     mainWindow.setMinimumSize(640, 480);
 
     // Check for first run
-    var configPath = app.getPath("userData") + "\\config.json";
     try {
         if (!fs.existsSync(configPath)) {
             console.log("Config file not found. Running initial setup.");
             initialSetup(true);
         } else {
-            if (verifyUnity()) {
-                showMainWindow();
-            } else {
-                installUnity(showMainWindow);
-            }
+            ensureUnity(showMainWindow);
         }
     } catch (ex) {
         dialog.showErrorBox(
@@ -121,8 +216,13 @@ app.on("ready", function () {
 });
 
 function showMainWindow() {
-    var configPath = app.getPath("userData") + "\\config.json";
-    var config = fs.readJsonSync(configPath);
+    config = fs.readJsonSync(configPath);
+
+    if (disableHTTPS) {
+        if (config["game-url"].startsWith("https")) {
+            config["game-url"] = config["game-url"].replace("https", "http");
+        }
+    }
 
     console.log("Game URL:", config["game-url"]);
     mainWindow.loadUrl(config["game-url"]);
@@ -145,11 +245,15 @@ function showMainWindow() {
 
     mainWindow.webContents.on("will-navigate", function (evt, url) {
         evt.preventDefault();
-        var configPath = app.getPath("userData") + "\\config.json";
-        var config = fs.readJsonSync(configPath);
 
-        if (!url.startsWith(config['game-url'])) {
-            require("shell").openExternal(url);
+        if (disableHTTPS) {
+            if (url.startsWith("https")) {
+                url = url.replace("https", "http");
+            }
+        }
+
+        if (!url.startsWith(config["game-url"])) {
+            shell.openExternal(url);
         } else {
             mainWindow.loadUrl(url);
             initialPageLoad = true;
@@ -158,12 +262,12 @@ function showMainWindow() {
 
     mainWindow.webContents.on("did-fail-load", function () {
         if (!initialPageLoad) {
-          dialog.showErrorBox(
-              "Error!",
-              "Could not load page. Check your Internet connection, and game-url inside config.json."
-          );
-          mainWindow.destroy();
-          app.quit();
+            dialog.showErrorBox(
+                "Error!",
+                "Could not load page. Check your Internet connection, and game-url inside config.json."
+            );
+            mainWindow.destroy();
+            app.quit();
         }
     });
 }
